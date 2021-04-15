7276c8b382a433690a4c8fc20ce1bf86fee342a7
martinmsaavedra <martin@bildung.uy>
2021-04-15 13:08:45 +0000

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,17 +7,31 @@ env.hosts = ['35.231.45.150', '34.74.42.82']
 
 
 def do_deploy(archive_path):
-    '''Distributes an archive to your web servers, using the function do_deploy'''
+    '''Distributes an archive to your web servers,
+    using the function do_deploy'''
     filename = archive_path[9:-4]
-   
+
     if not path.exists(archive_path):
         return False
-    put(archive_path, "/tmp/")
-    run("sudo mkdir -p /data/web_static/releases/{}/".format(filename))
-    run('sudo tar -zxf /tmp/{} -C /data/web_static/releases/{}/'.format(filename + ".tgz", filename))
-    run('sudo rm /tmp/{}'.format(filename + '.tgz'))
-    run('sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(filename, filename))
-    run('sudo rm -rf /data/web_static/releases/{}/web_static'.format(filename))
-    run('sudo rm -rf /data/web_static/current')
-    run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(filename))
-    
+    if put(archive_path, "/tmp/").failed:
+        return False
+    if run("sudo mkdir -p /data/web_static/releases/{}/"
+           .format(filename)).failed:
+        return False
+    if run('sudo tar -zxf /tmp/{} -C /data/web_static/releases/{}/'
+           .format(filename + ".tgz", filename)).failed:
+        return False
+    if run('sudo rm /tmp/{}'.format(filename + '.tgz')).failed:
+        return False
+    if run('sudo mv /data/web_static/releases/{}/web_static/*'
+            '/data/web_static/releases/{}/'.format(filename, filename)).failed:
+        return False
+    if run('sudo rm -rf /data/web_static/releases/{}/web_static'
+           .format(filename)).failed:
+        return False
+    if run('sudo rm -rf /data/web_static/current').failed:
+        return False
+    if run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'
+           .format(filename)).failed:
+        return False
+    return True
