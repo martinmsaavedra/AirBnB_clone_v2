cfdb467f5155652216a9af37e23f35377a073503
Martin Saavedra <martin@bildung.uy>
2021-05-02 13:11:53 +0000

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,10 +13,12 @@ def list_states():
     states_list = list(storage.all(State).values())
     return render_template('7-states_list.html', states_list=states_list)
 
+
 @app.teardown_appcontext
 def close_session(self):
     """function that call close method"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
