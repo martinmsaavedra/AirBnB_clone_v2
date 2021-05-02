6b68228a8288c94a9c136a4e0438a4cbd7792f55
Martin Saavedra <martin@bildung.uy>
2021-05-02 13:04:11 +0000

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
 '''Flask Module'''
 from flask import Flask, abort
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_world():
-    '''First Flask Function'''
-    return 'Hello HBNB!'
+def hello():
+    '''web application must be listening on 0.0.0.0, port 5000'''
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hello_HBNB():
-    '''Second Flask Function'''
+def hbnb():
+    '''new route'''
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def hello_c(text):
-    '''Third Flask Function'''
-    return "C {}".format(str(text)).replace("_", " ")
+def text(text):
+    '''new route with variable'''
+    return 'C {}'.format(str(text.replace('_', ' ')))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def hello_python(text="is cool"):
-    '''Forth Flask Function'''
-    return "Python {}".format(str(text)).replace("_", " ")
+def texts(text='is cool'):
+    '''new route with text variable'''
+    return 'Python {}'.format(str(text.replace('_', ' ')))
 
 
 @app.route('/number/<n>', strict_slashes=False)
-def hello_num(n):
-    '''Fifth Flask Function'''
+def number(n):
+    '''new route with int variable'''
     try:
-        return "{} is number".format(int(n))
+        return '{} is a number'.format(int(n))
     except:
-        return abort(404)
+        abort(404)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
